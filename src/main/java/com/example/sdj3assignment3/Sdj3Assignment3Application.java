3ceac3d11bb5a829a866b2a5571ea7afae957fb9
Merethe Lundgreen <299922@via.dk>
2022-11-03 11:35:57 +0100

    Initial commit

---
+++ src/main/java/com/example/sdj3assignment3/Sdj3Assignment3Application.java
@@ -0,0 +1,13 @@
+package com.example.sdj3assignment3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdj3Assignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdj3Assignment3Application.class, args);
+	}
+
+}
